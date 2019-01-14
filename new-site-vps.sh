f6377be23f2caaba0ca5ac5c5e4cc6a44e39289d
Luciano Closs <lcloss@users.noreply.github.com>
2019-01-14 01:40:44 +0000

    Fix some bugs

--- new-site-vps.sh
+++ new-site-vps.sh
@@ -3,22 +3,22 @@
 # Example:  ./new-site-on-vps new-domain.com other-domain.com
 # 
 # Create a path for new site
-sudo mkdir /var/www/$1/html
+sudo mkdir -R /var/www/$1/html
 
 # Set path onwer
-sudo chown $USER:$USER /var/www/$1/html
+sudo chown -R $USER:$USER /var/www/$1/html
 
 # Add write permission
-sudo chmod +755 /var/www/$1
+sudo chmod -R 755 /var/www/$1
 
 # Copy contents of source folder
 sudo cp /var/www/$2/html/*.* /var/www/$1/html/
 
 # Create vhost file from source
-sudo cp /etc/apache2/sites-enabled/$2.conf /etc/apache2/sites-enabled/$1.conf
+sudo cp /etc/apache2/sites-available/$2.conf /etc/apache2/sites-available/$1.conf
 
 # Replace domain on vhost
-sed -i 's/$2/$1/g' $1.conf
+sed -i 's/$2/$1/g' /etc/apache2/sites-available/$1.conf
 
 # Enable the site on vhost
-sudo a2ensite $1
+sudo a2ensite $1.conf
